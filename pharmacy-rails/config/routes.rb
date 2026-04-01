7c101bae26257df3e13f92304568af3451feebbf
andresaravia77-dev <andresaravia77@gmail.com>
2026-03-31 19:21:41 -0500

    Delete pharmacy-rails directory

--- pharmacy-rails/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  root "prescriptions#home"
-
-  post "/prescriptions/:id/instructions", to: "prescriptions#add_instructions"
-  get  "/prescriptions/:id/print",        to: "prescriptions#print"
-  get  "/prescriptions/:id/history",      to: "prescriptions#history"
-
-end
